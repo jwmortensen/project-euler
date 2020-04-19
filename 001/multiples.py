f0898215dc59f640784471f20e3739f99d57007f
Jacob Mortensen <jwmortensen@users.noreply.github.com>
2020-04-19 10:09:07 -0700

    update to use timeit

--- 001/multiples.py
+++ 001/multiples.py
@@ -1,19 +1,22 @@
 import timeit
 
-def pe1_brute_force(max_val):
+def multiples_brute_force(max_val):
 	total = 0
 	for val in range(max_val):
 		if val % 3 == 0 or val % 5 == 0:
 			total += val
 	return total
 
-print(pe1_brute_force(1000))
+print(multiples_brute_force(1000))
 
 
-def pe1(max_val):
+def multiples(max_val):
 	return sum([val for val in range(max_val) if val % 3 == 0 or val % 5 == 0])
 
-start_time = time.time()
-print(pe1(1000))
-end_time = time.time()
-print("%s seconds elapsed" % round(end_time - start_time, 6))
+
+print(multiples(1000))
+
+print("brute force time:")
+print(timeit.timeit("multiples_brute_force(1000)", number=10000, globals=globals()))
+print("list comprehension time:")
+print(timeit.timeit("multiples(1000)", number=10000, globals=globals()))
