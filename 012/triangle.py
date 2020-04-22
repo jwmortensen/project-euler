1b1fbb67fbde6cfbe1f6f01698f357e53c38b57a
Jacob Mortensen <jwmortensen@users.noreply.github.com>
2020-04-22 11:13:46 -0700

    improved solution for 012

--- 012/triangle.py
+++ 012/triangle.py
@@ -1,4 +1,5 @@
 from time import time
+import math
 
 
 def triangles():
@@ -8,12 +9,23 @@ def triangles():
 		yield triangle
 		n += 1
 		triangle += n
-		
+
+
 def get_factors(n):
 	vals = range(1, int((n + 2) / 2))
 	factors = [x for x in vals if n % x == 0]
 	factors.append(n)
-	return factors 
+	return factors
+
+
+def num_factors(y):
+	root_y = math.sqrt(y)
+	vals = range(1, int(root_y))
+	n_factors = 2 * sum(map(lambda x: y % x == 0, vals))
+	if root_y % 1 == 0:
+		n_factors += 1
+	return n_factors
+
 
 tri = triangles()
 
@@ -21,7 +33,7 @@ start = time()
 max_factors = (1, 1)
 while max_factors[1] < 500:
 	val = next(tri)
-	n_factors = len(get_factors(val))
+	n_factors = num_factors(val)
 	if n_factors > max_factors[1]:
 		max_factors = (val, n_factors)
 		print(max_factors)
