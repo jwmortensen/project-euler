18903d4b2f0ce1163f7bfafb9c827ed94d13abfa
Jacob Mortensen <jwmortensen@users.noreply.github.com>
2020-04-23 09:25:46 -0700

    solution to 017

--- 017/num_to_text.py
+++ 017/num_to_text.py
@@ -1,4 +1,5 @@
-mapping = {
+ones = {
+    0: "",
     1: "one",
     2: "two",
     3: "three",
@@ -7,23 +8,53 @@ mapping = {
     6: "six",
     7: "seven",
     8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninety",
+    9: "nine"
 }
+teens = {
+    0: "ten",
+    1: "eleven",
+    2: "twelve",
+    3: "thirteen",
+    4: "fourteen",
+    5: "fifteen",
+    6: "sixteen",
+    7: "seventeen",
+    8: "eighteen",
+    9: "nineteen"
+}
+tens = {
+    0: "",
+    2: "twenty",
+    3: "thirty",
+    4: "forty",
+    5: "fifty",
+    6: "sixty",
+    7: "seventy",
+    8: "eighty",
+    9: "ninety"
+}
+
+
+def num_to_string(num):
+    string = ""
+    hund = num // 100
+    ten = num % 100 // 10
+    one = num % 10
+    if hund > 0:
+        string += ones[hund] + "hundred"
+    if len(string) > 0 and (ten > 0 or one > 0):
+        string += "and"
+    if ten > 1:
+        string += tens[ten]
+    elif ten == 1:
+        string += teens[one]
+    if ten != 1:
+        string += ones[one]
+    return string
+
+
+number_name = "onethousand"
+for i in range(1, 1000):
+    number_name += num_to_string(i)
+print(len(number_name))
+
